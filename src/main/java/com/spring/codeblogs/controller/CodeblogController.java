721bc3bcd6c3e81cc380055db04afa4371c35f75
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-26 20:47:56 -0300

    Criando Detalhes do Post

--- src/main/java/com/spring/codeblogs/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblogs/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import com.spring.codeblogs.model.Post;
 import com.spring.codeblogs.service.CodeblogServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,6 +24,14 @@ public class CodeblogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts",posts);
+        return mv;
+    }
+
+    @RequestMapping(value = "/posts/{id}",method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable ("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post",post);
         return mv;
     }
 
