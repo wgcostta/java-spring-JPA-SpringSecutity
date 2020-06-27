15ebb71beb42919843ef4b0bb975653315a8420e
Wagner Costa <39952575+wgcostta@users.noreply.github.com>
2020-06-26 21:51:29 -0300

    Update CodeblogController.java

--- src/main/java/com/spring/codeblogs/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblogs/controller/CodeblogController.java
@@ -5,11 +5,15 @@ import com.spring.codeblogs.model.Post;
 import com.spring.codeblogs.service.CodeblogServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -36,11 +40,14 @@ public class CodeblogController {
     }
 
     @RequestMapping(value = "/newpost",method = RequestMethod.POST)
-    public ModelAndView setPostForm(@PathVariable ("id") long id){
-        ModelAndView mv = new ModelAndView("postDetails");
-//        codeblogService.save(Post post);
-//        mv.addObject("post",post);
-        return mv;
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        //Se ocorrer algum erro de validação.
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
     }
 
     @RequestMapping(value = "/newpost",method = RequestMethod.GET)
